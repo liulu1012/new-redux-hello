dfd171f88f915fcffd785cbba359b10105190d69
liulu1012 <645911726@qq.com>
2017-07-23 23:53:54 +0000

    dispath action

--- src/CommentBox.js
+++ src/CommentBox.js
@@ -5,10 +5,12 @@ class CommentBox extends React.Component{
   handleSubmit = (e) =>{
     e.preventDefault()
     let newComment = this.commentInput.value
+    console.log(store.getState())
+    store.dispath({type:'ADD_COMMENT',comment:newComment})
+    console.log(store.getState())
     this.commentForm.reset()
   }
   render(){
-    console.log(store.getState())
     let comments = store.getState()
     return(
       <div className='comment-box'>
