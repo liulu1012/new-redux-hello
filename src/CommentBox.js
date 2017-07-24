89b509fb3189aa14d7cff5c8476bef3fb92f61cc
liulu1012 <645911726@qq.com>
2017-07-24 00:10:48 +0000

    ADD_COMMENT works

--- src/CommentBox.js
+++ src/CommentBox.js
@@ -2,19 +2,25 @@ import React from 'react'
 import store from './store'
 
 class CommentBox extends React.Component{
+  state = {
+    comments:store.getState()
+  }
   handleSubmit = (e) =>{
     e.preventDefault()
     let newComment = this.commentInput.value
     console.log(store.getState())
     store.dispath({type:'ADD_COMMENT',comment:newComment})
     console.log(store.getState())
+    this.setState({
+      comments:store.getState()
+    })
     this.commentForm.reset()
   }
   render(){
     let comments = store.getState()
     return(
       <div className='comment-box'>
-        {comments.map(item=>(
+        {this.state.comments.map(item=>(
           <li key={Math.random()} className='comment'>{item}</li>
           ))
         }
